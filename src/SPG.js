13a2d640060a650d65a6670197d07958d8bd6e43
felipeurien <urienfelipe@gmail.com>
2023-01-02 23:07:18 -0300

    forms

--- src/SPG.js
+++ src/SPG.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -5,17 +6,33 @@ import LandingPage from "./views/LandingPage.jsx";
 import Services2D from "./views/Services2D.jsx";
 import Services3D from "./views/Services3D.jsx";
 import AppBar from "./components/AppBar";
+=======
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LandingPage from './views/LandingPage.jsx';
+import Services2D from './views/Services2D.jsx';
+import Services3D from './views/Services3D.jsx';
+import AppBar from './components/AppBar';
+import Footer from './components/Footer';
+import Forms from './components/Forms';
+>>>>>>> Stashed changes
 
 function SPG() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <AppBar />
         <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route exact path="/3D-configurators" element={<Services3D />} />
-          <Route exact path="/2D-configurators" element={<Services2D />} />
+          <Route exact path='/' element={<LandingPage />} />
+          <Route exact path='/3D-configurators' element={<Services3D />} />
+          <Route exact path='/2D-configurators' element={<Services2D />} />
         </Routes>
+<<<<<<< Updated upstream
+=======
+        <Forms />
+        <Footer />
+>>>>>>> Stashed changes
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Forms.jsx
@@ -0,0 +1,189 @@
+import React from 'react';
+import { Container, width } from '@mui/system';
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
+import ICON_FACEBOOK from '../assets/ICON_FACEBOOK.svg';
+import ICON_INSTAGRAM from '../assets/ICON_INSTAGRAM.svg';
+import ICON_LINKEDIN from '../assets/ICON_LINKEDIN.svg';
+import { BorderColor } from '@mui/icons-material';
+
+const styles = {
+  container: {
+    padding: 10,
+    backgroundColor: 'White',
+    borderRadius: '6px',
+  },
+  mainBox: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  textTitle: {
+    weight: '1200',
+  },
+  leftColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+  },
+  icons: {
+    width: '50px',
+    height: '50px',
+    color: 'red',
+    marginTop: '15px',
+    alignItems: 'center',
+    marginRight: '1.5rem',
+  },
+  iconsContact: {
+    width: '34px',
+    height: '34px',
+    color: 'white',
+    marginTop: '15px',
+    alignItems: 'center',
+    marginRight: '1.25rem',
+  },
+  textBigTitle: {
+    marginTop: '4rem',
+    marginBottom: '-4rem',
+  },
+  barra: {
+    height: '16rem',
+    border: '1px solid black',
+    width: '0px',
+    marginRight: '50px',
+    marginLeft: '25px',
+  },
+  inputs: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    textAlign: 'start',
+  },
+};
+
+export default function Forms() {
+  return (
+    <Container sx={styles.container}>
+      {/* TITULO */}
+      <Box sx={{ textAlign: 'start' }}>
+        <Typography variant='h7' gutterBottom>
+          LET US HELP YOU
+        </Typography>
+        <Typography variant='h4' gutterBottom>
+          TELL US YOUR IDEA
+        </Typography>
+      </Box>
+      <Box sx={styles.mainBox}>
+        {/* INFO (Phone - Email - Location) */}
+        <Box item xs={4} sx={styles.leftColumn}>
+          <Box sx={{ textAlign: 'start', display: 'flex' }}>
+            <Box>
+              <Typography mt={2} sx={styles.textTitle}>
+                Phone
+              </Typography>
+              <Typography sx={styles.text}>+01 (305) 790 - 2129</Typography>
+            </Box>
+          </Box>
+          <Box sx={{ display: 'flex' }}>
+            <Box sx={{ flexDirection: 'column' }}>
+              <Box sx={{ textAlign: 'start', display: 'flex' }}>
+                <Box>
+                  <Typography sx={styles.textTitle} mt={2}>
+                    Email
+                  </Typography>
+                  <Typography sx={styles.text}>
+                    Sebastian@spgdigitalcorp.com
+                  </Typography>
+                </Box>
+              </Box>
+              <Box sx={{ textAlign: 'start', display: 'flex' }}>
+                <Box>
+                  <Typography sx={styles.textTitle} mt={2}>
+                    Location
+                  </Typography>
+                  <Typography sx={styles.text}>
+                    550 ocean 2c drive key Biscayne 33149
+                  </Typography>
+                </Box>
+              </Box>
+              <Box sx={{ textAlign: 'start', marginBottom: '0.25rem' }}>
+                <a href='https://www.instagram.com/spg_digital/'>
+                  <Box component='img' src={ICON_INSTAGRAM} sx={styles.icons} />
+                </a>
+                <a href='https://www.linkedin.com/company/spg-digital/about//'>
+                  <Box component='img' src={ICON_LINKEDIN} sx={styles.icons} />
+                </a>
+                <a href='https://www.instagram.com/spg_digital/'>
+                  <Box component='img' src={ICON_FACEBOOK} sx={styles.icons} />
+                </a>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        {/* BARRA */}
+        <Box sx={styles.barra} />
+        {/* INPUT */}
+        <Box sx={styles.inputs}>
+          <Box sx={{}}>
+            <TextField
+              sx={{ marginRight: '2rem' }}
+              id='standard-helperText'
+              label='Your e-mail'
+              variant='outlined'
+            />
+            <TextField
+              id='standard-helperText'
+              label='Your name'
+              variant='outlined'
+            />
+          </Box>
+          <Box>
+            <TextField
+              sx={{ marginTop: '25px', marginBottom: '20px' }}
+              id='outlined-multiline-static'
+              label='How can we help you?'
+              multiline
+              rows={3}
+              fullWidth
+            />
+          </Box>
+          <FormControlLabel
+            sx={{ marginBottom: '25px' }}
+            control={
+              <Checkbox
+                defaultChecked
+                sx={{
+                  '&.Mui-checked': {
+                    color: 'gray',
+                  },
+                }}
+              />
+            }
+            label='Subscribe to newsletter'
+          />
+          <Button
+            sx={{
+              width: '175px',
+              borderRadius: '20px',
+              color: 'gray',
+              borderColor: 'gray',
+              '&:hover': {
+                borderColor: 'black',
+                color: 'black',
+              },
+            }}
+            variant='outlined'
+          >
+            Send!
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
