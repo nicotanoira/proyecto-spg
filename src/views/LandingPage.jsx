c7cf650b79cc80aee687b6207588abc5ee81adb5
felipeurien <urienfelipe@gmail.com>
2023-01-03 21:02:22 -0300

    arreglos en forms

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,24 +1,24 @@
-
-import { Box } from "@mui/system";
-import React from "react";
-import Brands from "../components/Brands";
-import Values from "../components/Values";
-
+import { Box } from '@mui/system';
+import React from 'react';
+import Brands from '../components/Brands';
+import Values from '../components/Values';
+import Forms from '../components/Forms';
 
 export default function LandingPage() {
-	const styles = {
-		container: {
-			// height: "15rem",
-			// display: "flex",
-			// alignItems: "center",
-			// justifyContent: "center",
-			// flexDirection: "column",
-		},
-	};
-	return (
-		<Box sx={styles.container}>
-			<Brands />
-			<Values /> 
-		</Box>
-	);
+  const styles = {
+    container: {
+      // height: "15rem",
+      // display: "flex",
+      // alignItems: "center",
+      // justifyContent: "center",
+      // flexDirection: "column",
+    },
+  };
+  return (
+    <Box sx={styles.container}>
+      <Brands />
+      <Values />
+      <Forms />
+    </Box>
+  );
 }
