5799007661b067edd8e66d610bcd6322dc8bf57b
felipeurien <urienfelipe@gmail.com>
2023-01-05 23:46:46 -0300

    Customers Experience terminado (?

--- src/views/Services3D.jsx
+++ src/views/Services3D.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { Box, Container } from '@mui/system';
+import React from 'react';
+import CustomerExp from '../components/CustomersExp';
 
 export default function Services3D() {
   return (
-    <div>es3D</div>
-  )
+    <Box>
+      <CustomerExp />
+    </Box>
+  );
 }
