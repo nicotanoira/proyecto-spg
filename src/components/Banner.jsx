bf4b2989976fc3ab2404bf645d76d311607ae156
felipeurien <urienfelipe@gmail.com>
2023-01-26 16:20:19 -0300

    mail

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,165 +1,162 @@
-import React from "react";
-import { Box, Container } from "@mui/system";
-import BannerWeb from "../assets/banner_web-01.png";
-import BannerMobile from "../assets/banner-mobile.png";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import React from 'react';
+import { Box, Container } from '@mui/system';
+import BannerWeb from '../assets/banner_web-01.png';
+import BannerMobile from '../assets/banner-mobile.png';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import {
-	Button,
-	IconButton,
-	Typography,
-	useMediaQuery,
-	useTheme,
-} from "@mui/material";
+  Button,
+  IconButton,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 
 export default function Banner() {
-	const theme = useTheme();
-	const down600px = useMediaQuery(theme.breakpoints.down("sm"));
-	const down900px = useMediaQuery(theme.breakpoints.down("md"));
-	const down1200px = useMediaQuery(theme.breakpoints.down("lg"));
-	const styles = {
-		container: {
-			padding: "0 0 ",
+  const theme = useTheme();
+  const down600px = useMediaQuery(theme.breakpoints.down('sm'));
+  const down900px = useMediaQuery(theme.breakpoints.down('md'));
+  const down1200px = useMediaQuery(theme.breakpoints.down('lg'));
+  const styles = {
+    container: {
+      padding: '0 0 ',
 
-			position: "relative",
-			padding: 0,
-			display: "flex",
-			alignItems: "start",
-			justifyContent: "end",
-			"@media (min-width: 600px)": {
-				padding: 0,
-			},
-		},
-		image: {
-			width: "100%",
-			// height: down600px && "80vh",
-			padding: "0",
-		},
-		textBox: {
-			position: "absolute",
-			display: "flex",
-			flexDirection: "column",
-			alignItems: { xs: "center", md: "end" },
-			justifyContent: { xs: "center" },
-			padding: { xs: "2rem" },
-			paddingRight: { xs: "2rem", md: "10vw" },
-			marginTop: { xs: "1vh", sm: "4vh", md: "8vh", lg: "13vh"},
-			// right: "8vw",
-			// top: "13vw",
-			textAlign: { xs: "center", md: "end" },
-		},
-		button: {
-			backgroundColor: "#273339",
-			borderRadius: "1000px",
-			fontSize: "10px",
-			padding: "6px 10px",
-			color: "#ffffffce",
-			textTransform: "capitalize",
-			width: "10rem",
-		},
-		icon: { marginLeft: "2px", transform: "scale(.8)" },
-	};
+      position: 'relative',
+      padding: 0,
+      display: 'flex',
+      alignItems: 'start',
+      justifyContent: 'end',
+      '@media (min-width: 600px)': {
+        padding: 0,
+      },
+    },
+    image: {
+      width: '100%',
+      // height: down600px && "80vh",
+      padding: '0',
+    },
+    textBox: {
+      position: 'absolute',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: { xs: 'center', md: 'end' },
+      justifyContent: { xs: 'center' },
+      padding: { xs: '2rem' },
+      paddingRight: { xs: '2rem', md: '10vw' },
+      marginTop: { xs: '1vh', sm: '4vh', md: '8vh', lg: '13vh' },
+      // right: "8vw",
+      // top: "13vw",
+      textAlign: { xs: 'center', md: 'end' },
+    },
+    button: {
+      backgroundColor: '#273339',
+      borderRadius: '1000px',
+      fontSize: '10px',
+      padding: '6px 10px',
+      color: '#ffffffce',
+      textTransform: 'capitalize',
+      width: '10rem',
+    },
+    icon: { marginLeft: '2px', transform: 'scale(.8)' },
+  };
 
-	return (
-		<Container maxWidth="100vw" sx={styles.container}>
-			<Box sx={styles.textBox}>
-				<Box sx={styles.upperTextBox}>
-					<Typography
-						variant="h5"
-						sx={{
-							marginRight: { xs: "" },
-							fontSize: down900px ? "12px" : down1200px ? "16px" : "24px",
-							fontWeight: "400",
-							color: "#273339",
-						}}
-						gutterBottom
-					>
-						DIGITAL AGENCY
-					</Typography>
-				</Box>
+  return (
+    <Container maxWidth='100vw' sx={styles.container}>
+      <Box sx={styles.textBox}>
+        <Box sx={styles.upperTextBox}>
+          <Typography
+            variant='h5'
+            sx={{
+              marginRight: { xs: '' },
+              fontSize: down900px ? '12px' : down1200px ? '16px' : '24px',
+              fontWeight: '400',
+              color: '#273339',
+            }}
+            gutterBottom
+          >
+            DIGITAL AGENCY
+          </Typography>
+        </Box>
 
-				<Box sx={styles.middleTextBox}>
-					{down600px ? (
-						<Typography
-						variant="h2"
-						sx={{
-							height: "fit-content",
-							fontSize: down900px ? "25px" : down1200px ? "55px" : "74px",
-							fontWeight: 500,
-							color: "#273339",
-						}}
-						gutterBottom
-					>
-						TAILORING SOLUTIONS
-					</Typography>
-					) : (
-					<Typography
-						variant="h2"
-						sx={{
-							height: "fit-content",
-							fontSize: down900px ? "30px" : down1200px ? "55px" : "74px",
-							fontWeight: 500,
-							color: "#273339",
-						}}
-						gutterBottom
-					>
-						TAILORING
-						<br />
-						SOLUTIONS
-					</Typography>
+        <Box sx={styles.middleTextBox}>
+          {down600px ? (
+            <Typography
+              variant='h2'
+              sx={{
+                height: 'fit-content',
+                fontSize: down900px ? '25px' : down1200px ? '55px' : '74px',
+                fontWeight: 500,
+                color: '#273339',
+              }}
+              gutterBottom
+            >
+              TAILORING SOLUTIONS
+            </Typography>
+          ) : (
+            <Typography
+              variant='h2'
+              sx={{
+                height: 'fit-content',
+                fontSize: down900px ? '30px' : down1200px ? '55px' : '74px',
+                fontWeight: 500,
+                color: '#273339',
+              }}
+              gutterBottom
+            >
+              TAILORING
+              <br />
+              SOLUTIONS
+            </Typography>
+          )}
+        </Box>
 
-					)
+        <Box sx={styles.lowerTextBox}>
+          {!down600px ? (
+            <Typography
+              variant='body1'
+              sx={{
+                fontSize: down900px ? '6px' : down1200px ? '18px' : '25px',
+                fontWeight: 400,
+                color: '#273339',
+              }}
+            >
+              We have the power to help you strengthen
+              <br />
+              your customers' shopping experience
+            </Typography>
+          ) : (
+            <>
+              <Typography
+                variant='body1'
+                sx={{
+                  fontSize: down900px ? '13px' : down1200px ? '1.3rem' : '2rem',
+                  marginBottom: { xs: '12px' },
+                  fontWeight: 400,
+                  color: '#273339',
+                }}
+              >
+                We have the power to help you strengthen your customers'
+                shopping experience creating innovative product imaging
+                solutions.
+              </Typography>
+              <IconButton
+                size='small'
+                endIcon
+                variant='contained'
+                sx={styles.button}
+              >
+                Contact Us
+                <MailOutlineIcon sx={styles.icon} fontSize='small' />
+              </IconButton>
+            </>
+          )}
+        </Box>
+      </Box>
 
-					}
-				</Box>
-
-				<Box sx={styles.lowerTextBox}>
-					{!down600px ? (
-						<Typography
-							variant="body1"
-							sx={{
-								fontSize: down900px ? "6px" : down1200px ? "18px" : "25px",
-								fontWeight: 400,
-								color: "#273339",
-							}}
-						>
-							We have the power to help you strengthen
-							<br />
-							your customers' shopping experience
-						</Typography>
-					) : (
-						<>
-							<Typography
-								variant="body1"
-								sx={{
-									fontSize: down900px ? "13px" : down1200px ? "1.3rem" : "2rem",
-									marginBottom: { xs: "12px" },
-									fontWeight: 400,
-									color: "#273339",
-								}}
-							>
-								We have the power to help you strengthen your customers'
-								shopping experience creating innovative product imaging
-								solutions.
-							</Typography>
-							<IconButton
-								size="small"
-								endIcon
-								variant="contained"
-								sx={styles.button}
-							>
-								Contact Us
-								<MailOutlineIcon sx={styles.icon} fontSize="small" />
-							</IconButton>
-						</>
-					)}
-				</Box>
-			</Box>
-
-			<Box
-				component="img"
-				src={!down600px ? BannerWeb : BannerMobile}
-				sx={styles.image}
-			/>
-		</Container>
-	);
+      <Box
+        component='img'
+        src={!down600px ? BannerWeb : BannerMobile}
+        sx={styles.image}
+      />
+    </Container>
+  );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,235 +1,287 @@
-import React from "react";
-import { Container, width } from "@mui/system";
+import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react';
+import { Container, width } from '@mui/system';
 import {
-	Box,
-	Button,
-	Checkbox,
-	FormControlLabel,
-	Grid,
-	TextField,
-	Typography,
-	useMediaQuery,
-	useTheme,
-} from "@mui/material";
-import ICON_FACEBOOK from "../assets/facebookoscuro.svg";
-import ICON_INSTAGRAM from "../assets/instagramoscuro.svg";
-import ICON_LINKEDIN from "../assets/linkedinoscuro.svg";
-import { BorderColor } from "@mui/icons-material";
+    Box,
+    Button,
+    Checkbox,
+    FormControlLabel,
+    Grid,
+    TextField,
+    Typography,
+    useMediaQuery,
+    useTheme,
+} from '@mui/material';
+import ICON_FACEBOOK from '../assets/facebookoscuro.svg';
+import ICON_INSTAGRAM from '../assets/instagramoscuro.svg';
+import ICON_LINKEDIN from '../assets/linkedinoscuro.svg';
+import { BorderColor, ChevronLeft } from '@mui/icons-material';
 
 const styles = {
-	container: {
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-		backgroundColor: "White",
-		borderRadius: "6px",
-		margin: "8.5rem 0",
-	},
-	mainBox: {
-		textAlign: "start",
-		display: "flex",
-		flexDirection: "row",
-	},
-	textTitle: {
-		weight: "1200",
-	},
-	leftColumn: {
-		display: "flex",
-		flexDirection: "column",
-		flexWrap: "wrap",
-	},
-	icons: {
-		width: "50px",
-		height: "50px",
-		color: "red",
-		marginTop: "15px",
-		alignItems: "center",
-		marginRight: "1.5rem",
-	},
-	iconsContact: {
-		width: "34px",
-		height: "34px",
-		color: "white",
-		marginTop: "15px",
-		alignItems: "center",
-		marginRight: "1.25rem",
-	},
-	textBigTitle: {
-		marginTop: "4rem",
-		marginBottom: "-4rem",
-	},
-	barra: {
-		height: "18rem",
-		border: "1px solid black",
-		width: "0px",
-		margin: "0 10rem 0 9rem",
-	},
-	inputs: {
-		display: "flex",
-		flexDirection: "column",
-		flexWrap: "wrap",
-		textAlign: "start",
-	},
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'White',
+        borderRadius: '6px',
+        margin: '8.5rem 0',
+    },
+    mainBox: {
+        textAlign: 'start',
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    textTitle: {
+        weight: '1200',
+    },
+    leftColumn: {
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+    },
+    icons: {
+        width: '50px',
+        height: '50px',
+        color: 'red',
+        marginTop: '15px',
+        alignItems: 'center',
+        marginRight: '1.5rem',
+    },
+    iconsContact: {
+        width: '34px',
+        height: '34px',
+        color: 'white',
+        marginTop: '15px',
+        alignItems: 'center',
+        marginRight: '1.25rem',
+    },
+    textBigTitle: {
+        marginTop: '4rem',
+        marginBottom: '-4rem',
+    },
+    barra: {
+        height: '18rem',
+        border: '1px solid black',
+        width: '0px',
+        margin: '0 10rem 0 9rem',
+    },
+    inputs: {
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        textAlign: 'start',
+    },
 };
 
 export default function Contact() {
-	const theme = useTheme();
-	const down900px = useMediaQuery(theme.breakpoints.down("md"));
-	return (
-		<Container sx={styles.container}>
-			{/* TITULO */}
-			<Box sx={{ flexDirection: "column" }}>
-				<Box sx={{ textAlign: "start" }}>
-					<Typography
-						sx={{ fontSize: { xs: "1rem", md: "1.25rem" } }}
-						gutterBottom
-					>
-						LET US HELP YOU
-					</Typography>
-					<Typography
-						sx={{ fontSize: { xs: "1.8rem", md: "3rem" } }}
-						gutterBottom
-					>
-						TELL US YOUR IDEA
-					</Typography>
-				</Box>
-				<Box sx={styles.mainBox}>
-					{/* INFO (Phone - Email - Location) */}
-					{!down900px && (
-						<Box item xs={4} sx={styles.leftColumn}>
-							<Box sx={{ textAlign: "start", display: "flex" }}>
-								<Box>
-									<Typography mt={2} sx={styles.textTitle}>
-										Phone
-									</Typography>
-									<Typography sx={styles.text}>+01 (305) 790 - 2129</Typography>
-								</Box>
-							</Box>
-							<Box sx={{ display: "flex" }}>
-								<Box sx={{ flexDirection: "column" }}>
-									<Box sx={{ textAlign: "start", display: "flex" }}>
-										<Box>
-											<Typography sx={styles.textTitle} mt={2}>
-												Email
-											</Typography>
-											<Typography sx={styles.text}>
-												Sebastian@spgdigitalcorp.com
-											</Typography>
-										</Box>
-									</Box>
-									<Box sx={{ textAlign: "start", display: "flex" }}>
-										<Box>
-											<Typography sx={styles.textTitle} mt={2}>
-												Location
-											</Typography>
-											<Typography sx={styles.text}>
-												550 ocean 2c drive key Biscayne 33149
-											</Typography>
-										</Box>
-									</Box>
-									<Box sx={{ textAlign: "start", marginBottom: "0.25rem" }}>
-										<a
-											target="_blank"
-											href="https://www.instagram.com/spg_digital/"
-										>
-											<Box
-												component="img"
-												src={ICON_INSTAGRAM}
-												sx={styles.icons}
-											/>
-										</a>
-										<a
-											target="_blank"
-											href="https://www.linkedin.com/company/spg-digital/about//"
-										>
-											<Box
-												component="img"
-												src={ICON_LINKEDIN}
-												sx={styles.icons}
-											/>
-										</a>
-										<a
-											target="_blank"
-											href="https://www.instagram.com/spg_digital/"
-										>
-											<Box
-												component="img"
-												src={ICON_FACEBOOK}
-												sx={styles.icons}
-											/>
-										</a>
-									</Box>
-								</Box>
-							</Box>
-						</Box>
-					)}
-					{/* BARRA */}
+    const theme = useTheme();
+    const down900px = useMediaQuery(theme.breakpoints.down('md'));
+    const serviceID = 'service_9ytf5z9';
+    const templateID = 'template_gqo9571';
+    const publicKey = 'XJHP8cVGvoyOfhWum';
 
-					{!down900px && <Box sx={styles.barra} />}
-					{/* INPUT */}
-					<Box sx={styles.inputs}>
-						<Box sx={{}}>
-							<TextField
-								sx={{ marginRight: "2rem" }}
-								id="standard-helperText"
-								label="Your e-mail"
-								variant="outlined"
-							/>
+    const form = useRef();
 
-							{!down900px && (
-								<TextField
-									id="standard-helperText"
-									label="Your name"
-									variant="outlined"
-								/>
-							)}
-						</Box>
-						<Box>
-							<TextField
-								sx={{ marginTop: "25px", marginBottom: "20px" }}
-								id="outlined-multiline-static"
-								label="How can we help you?"
-								multiline
-								rows={3}
-								fullWidth
-							/>
-						</Box>
-						<FormControlLabel
-							sx={{ marginBottom: "25px" }}
-							control={
-								<Checkbox
-									defaultChecked
-									sx={{
-										"&.Mui-checked": {
-											color: "gray",
-										},
-									}}
-								/>
-							}
-							label="Subscribe to newsletter"
-						/>
-						<Box sx={{ textAlign: { xs: "center", md: "start" } }}>
-							<Button
-								sx={{
-									width: "175px",
-									borderRadius: "20px",
-									color: "gray",
-									borderColor: "gray",
-									textTransform: "capitalize",
-									textAlign: "center",
-									"&:hover": {
-										borderColor: "black",
-										color: "black",
-									},
-								}}
-								variant="outlined"
-							>
-								Send!
-							</Button>
-						</Box>
-					</Box>
-				</Box>
-			</Box>
-		</Container>
-	);
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(serviceID, templateID, form.current, publicKey).then(
+            (result) => {
+                console.log(result.text);
+                console.log('message sent');
+            },
+            (error) => {
+                console.log(error.text);
+            }
+        );
+    };
+
+    return (
+        <Container sx={styles.container}>
+            {/* TITULO */}
+            <Box sx={{ flexDirection: 'column' }}>
+                <Box sx={{ textAlign: 'start' }}>
+                    <Typography
+                        sx={{ fontSize: { xs: '1rem', md: '1.25rem' } }}
+                        gutterBottom
+                    >
+                        LET US HELP YOU
+                    </Typography>
+                    <Typography
+                        sx={{ fontSize: { xs: '1.8rem', md: '3rem' } }}
+                        gutterBottom
+                    >
+                        TELL US YOUR IDEA
+                    </Typography>
+                </Box>
+                <Box sx={styles.mainBox}>
+                    {/* INFO (Phone - Email - Location) */}
+                    {!down900px && (
+                        <Box item xs={4} sx={styles.leftColumn}>
+                            <Box sx={{ textAlign: 'start', display: 'flex' }}>
+                                <Box>
+                                    <Typography mt={2} sx={styles.textTitle}>
+                                        Phone
+                                    </Typography>
+                                    <Typography sx={styles.text}>
+                                        +01 (305) 790 - 2129
+                                    </Typography>
+                                </Box>
+                            </Box>
+                            <Box sx={{ display: 'flex' }}>
+                                <Box sx={{ flexDirection: 'column' }}>
+                                    <Box
+                                        sx={{
+                                            textAlign: 'start',
+                                            display: 'flex',
+                                        }}
+                                    >
+                                        <Box>
+                                            <Typography
+                                                sx={styles.textTitle}
+                                                mt={2}
+                                            >
+                                                Email
+                                            </Typography>
+                                            <Typography sx={styles.text}>
+                                                Sebastian@spgdigitalcorp.com
+                                            </Typography>
+                                        </Box>
+                                    </Box>
+                                    <Box
+                                        sx={{
+                                            textAlign: 'start',
+                                            display: 'flex',
+                                        }}
+                                    >
+                                        <Box>
+                                            <Typography
+                                                sx={styles.textTitle}
+                                                mt={2}
+                                            >
+                                                Location
+                                            </Typography>
+                                            <Typography sx={styles.text}>
+                                                550 ocean 2c drive key Biscayne
+                                                33149
+                                            </Typography>
+                                        </Box>
+                                    </Box>
+                                    <Box
+                                        sx={{
+                                            textAlign: 'start',
+                                            marginBottom: '0.25rem',
+                                        }}
+                                    >
+                                        <a
+                                            target='_blank'
+                                            href='https://www.instagram.com/spg_digital/'
+                                        >
+                                            <Box
+                                                component='img'
+                                                src={ICON_INSTAGRAM}
+                                                sx={styles.icons}
+                                            />
+                                        </a>
+                                        <a
+                                            target='_blank'
+                                            href='https://www.linkedin.com/company/spg-digital/about//'
+                                        >
+                                            <Box
+                                                component='img'
+                                                src={ICON_LINKEDIN}
+                                                sx={styles.icons}
+                                            />
+                                        </a>
+                                        <a
+                                            target='_blank'
+                                            href='https://www.instagram.com/spg_digital/'
+                                        >
+                                            <Box
+                                                component='img'
+                                                src={ICON_FACEBOOK}
+                                                sx={styles.icons}
+                                            />
+                                        </a>
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Box>
+                    )}
+                    {/* BARRA */}
+
+                    {!down900px && <Box sx={styles.barra} />}
+                    {/* INPUT */}
+                    <form sx={styles.inputs} ref={form} onSubmit={sendEmail}>
+                        <Box sx={{}}>
+                            <TextField
+                                sx={{ marginRight: '2rem' }}
+                                id='standard-helperText'
+                                label='Your e-mail'
+                                variant='outlined'
+                                type='email'
+                                name='email_id'
+                            />
+
+                            {!down900px && (
+                                <TextField
+                                    id='standard-helperText'
+                                    label='Your name'
+                                    variant='outlined'
+                                    type='text'
+                                    name='user_name'
+                                />
+                            )}
+                        </Box>
+                        <Box>
+                            <TextField
+                                sx={{ marginTop: '25px', marginBottom: '20px' }}
+                                id='outlined-multiline-static'
+                                label='How can we help you?'
+                                multiline
+                                rows={3}
+                                fullWidth
+                                name='message'
+                            />
+                        </Box>
+                        <FormControlLabel
+                            sx={{ marginBottom: '25px' }}
+                            control={
+                                <Checkbox
+                                    defaultChecked
+                                    sx={{
+                                        '&.Mui-checked': {
+                                            color: 'gray',
+                                        },
+                                    }}
+                                />
+                            }
+                            label='Subscribe to newsletter'
+                        />
+                        <Box sx={{ textAlign: { xs: 'center', md: 'start' } }}>
+                            <Button
+                                type='submit'
+                                value='Send'
+                                sx={{
+                                    width: '175px',
+                                    borderRadius: '20px',
+                                    color: 'gray',
+                                    borderColor: 'gray',
+                                    textTransform: 'capitalize',
+                                    textAlign: 'center',
+                                    '&:hover': {
+                                        borderColor: 'black',
+                                        color: 'black',
+                                    },
+                                }}
+                                variant='outlined'
+                            >
+                                Send!
+                            </Button>
+                        </Box>
+                    </form>
+                </Box>
+            </Box>
+        </Container>
+    );
 }
